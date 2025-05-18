872b0693edd1856d61b4dfeb49c3c55a85cca700
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-05-18 21:12:08 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+function App() {
+
+  const [firstName,setfirstName]=useState("");
+  const [lastName,setlastName]=useState("");
+  const [showMessage,setShowMessage]=useState(false);
+
+
+  const handlefirstName=(e)=>{
+    setfirstName(e.target.value);
+  }
+
+  const handlelastName=(e)=>{
+    setlastName(e.target.value);
+  }
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setShowMessage(true);
+  }
+
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+      <label>First Name:
+        <input
+          type="text" 
+          value={firstName}
+          required
+          onChange={e => handlefirstName(e)}
+        />
+      </label>
+      <br />
+
+      <label>Last Name:
+        <input
+          type="text" 
+          value={lastName}
+          required
+          onChange={(e) => handlelastName(e)}
+        />
+      </label>
+       <button style={{display:"block"}}>Submit</button>
+    </form>
+   
+    <p>{showMessage &&  `Full Name: ${firstName} ${lastName}`}</p>
+    </>
+  )
+}
+
+export default App
